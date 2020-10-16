3190607efc3c7c88f0a204f1ae1f8924c1ec5436
mateusz1414 <mateus141423@gmail.com>
2020-10-16 20:16:51 +0200

    edycja studenta

--- main.go
+++ main.go
@@ -4,8 +4,9 @@ import (
 	"database/sql"
 	"fmt"
 	"html/template"
-	"net/http"
+	"log"
 	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
@@ -14,11 +15,13 @@ import (
 var tmpl = template.Must(template.ParseFiles("templates/index.html"))
 
 type Studenci struct {
-	Imie     string
-	Nazwisko string
-	Data     string
-	Wydzial  string
-	Plec     string
+	Imie      string
+	Nazwisko  string
+	Data      string
+	Wydzial   string
+	Plec      string
+	Id        int
+	CzyEdycja bool
 }
 
 func connection() (*sql.DB, error) {
@@ -27,27 +30,27 @@ func connection() (*sql.DB, error) {
 	return sql.Open(typ, file)
 }
 
-func indexHandler(c *gin.Context) {
+func pobierzStudentow(idDoZmiany int) []Studenci {
 	database, err := connection()
 	if err != nil {
-		c.Writer.WriteHeader(http.StatusInternalServerError)
-		return
+		fmt.Errorf("Problem z polaczeniem %s", err.Error())
+		return nil
 	}
 	defer database.Close()
-	zapytanie, err := database.Query("SELECT imie,nazwisko,data_urodzenia,wydzial,plec from studenci")
+	zapytanie, err := database.Query("SELECT imie,nazwisko,data_urodzenia,wydzial,plec,id_studenta from studenci")
 	if err != nil {
-		c.Writer.WriteHeader(http.StatusInternalServerError)
-		return
+		fmt.Errorf("Problem z polaczeniem %s", err.Error())
+		return nil
 	}
-	var student = Studenci{}
+	var student = &Studenci{}
 	var studenci = []Studenci{}
 	for zapytanie.Next() {
 		var imie, nazwisko, data, wydzial string
-		var plec int
-		err = zapytanie.Scan(&imie, &nazwisko, &data, &wydzial, &plec)
+		var plec, id int
+		err = zapytanie.Scan(&imie, &nazwisko, &data, &wydzial, &plec, &id)
 		if err != nil {
-			c.Writer.WriteHeader(http.StatusInternalServerError)
-			return
+			fmt.Errorf("Problem z polaczeniem %s", err.Error())
+			return nil
 		}
 		var p string
 		if plec == 0 {
@@ -61,9 +64,57 @@ func indexHandler(c *gin.Context) {
 		student.Wydzial = wydzial
 		student.Data = data
 		student.Plec = p
-		studenci = append(studenci, student)
+		student.Id = id
+		if idDoZmiany == student.Id {
+			student.CzyEdycja = true
+		} else {
+			student.CzyEdycja = false
+		}
+		studenci = append(studenci, *student)
+
 	}
+	return studenci
+}
+
+func indexHandler(c *gin.Context) {
+	studenci := pobierzStudentow(0)
 	c.HTML(200, "index.html", studenci)
+}
+
+func studentEdit(c *gin.Context) {
+	id, iftrue := c.GetQuery("id")
+	idNumber, err := strconv.Atoi(id)
+	if err != nil {
+		log.Fatal("Problem z zamiana")
+		return
+	}
+	if iftrue {
+		studenci := pobierzStudentow(idNumber)
+		c.HTML(200, "index.html", studenci)
+	}
+}
+
+func studentChange(c *gin.Context) {
+	id := c.PostForm("id")
+	imie := c.PostForm("imie")
+	nazwisko := c.PostForm("nazwisko")
+	wydzial := c.PostForm("wydzial")
+	data := c.PostForm("data")
+	plec := c.PostForm("plec")
+	database, err := connection()
+	if err != nil {
+		fmt.Errorf("Problem z polaczeniem %s", err.Error())
+		return
+	}
+	zapytanie, _ := database.Prepare("UPDATE studenci SET imie=?,nazwisko=?,wydzial=?,data_urodzenia=?,plec=? WHERE id_studenta=?")
+	_, err = zapytanie.Exec(imie, nazwisko, wydzial, data, plec, id)
+	fmt.Println("czy to tu!!!!!!!!!!!!!!!!!!!!!!!!!")
+	if err != nil {
+		//fmt.Errorf("Problem z zapytaniem %s", err.Error())
+		return
+	}
+	c.Redirect(301, "/")
+
 }
 
 func main() {
@@ -72,6 +123,8 @@ func main() {
 	server.SetHTMLTemplate(tmpl)
 	server.Static("/assets", "./css")
 	server.GET("/", indexHandler)
+	server.GET("/edit", studentEdit)
+	server.POST("/changestudent", studentChange)
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
