0f95d9ab694a63fefe47b409cd868911e6d2a0a8
mateusz1414 <mateus141423@gmail.com>
2020-10-22 11:16:22 +0200

    działające package

--- main.go
+++ main.go
@@ -3,159 +3,51 @@ package main
 import (
 	"fmt"
 	"os"
+	"log"
+	"pierwszy/studenci"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-type Results struct {
-	Status       int
-	TotalResults int
-	Student      []Students
-}
 
-type Outs struct {
-	Status     int
-	Message    string
-	UpdateRows int64
-	ErrorValue string
-}
 
-type Students struct {
-	IDStudenta    int    `json:"idstudenta"`
-	Imie          string `json:"imiestudenta"`
-	Nazwisko      string `json:"nazwiskostudenta"`
-	DataUrodzenia string `json:"datastudenta"`
-	Wydzial       string `json:"wydzialstudenta"`
-	Plec          string `json:"plecstudenta"`
-}
-
-func connection() (*gorm.DB, error) {
-	file := "uczelnia"
-	return gorm.Open(sqlite.Open(file), &gorm.Config{})
-}
-
-func pobierzStudentow() []Students {
-	database, err := connection()
-	if err != nil {
-		panic("failed to connect database")
-	}
-	//To Twoszr tabele w bazie danych wg struktury
-	//database.AutoMigrate(&Students{})
-	//defer database.Close()
-	var studenci []Students
-	database.Find(&studenci)
-
-	return studenci
-}
-
-func indexHandler(c *gin.Context) {
-	studenci := pobierzStudentow()
-	RowsAffected := len(studenci)
-	result := Results{
-		Status:       200,
-		TotalResults: RowsAffected,
-		Student:      studenci,
-	}
-	c.JSON(200, result)
-}
-
-func studentDelete(c *gin.Context) {
-	var usunSe = Students{}
-	err := c.ShouldBindJSON(&usunSe)
-	if err != nil {
-		outs := Outs{
-			Status:     400,
-			Message:    "Podaj poprawny format danych",
-			UpdateRows: 0,
-			ErrorValue: err.Error(),
-		}
-		c.JSON(400, outs)
-		return
-	}
-	database, err := connection()
-	if err != nil {
-		fmt.Errorf("Problem z polaczeniem %s", err.Error())
-		return
-	}
-	result := database.Delete(&Students{}, &usunSe)
-	if result.Error != nil {
-		c.JSON(400, Outs{
-			Status:     400,
-			Message:    "Problem z usunieciem użytkownika",
-			UpdateRows: result.RowsAffected,
-			ErrorValue: result.Error.Error(),
-		})
-	}
-	c.JSON(200, Outs{
-		Status:     200,
-		Message:    "Usunieto użytkownika",
-		UpdateRows: result.RowsAffected,
-		ErrorValue: "",
-	})
-}
-
-func studentChange(c *gin.Context) {
-	//var edytujSe := Students{}
-	//err :=
-	//database, err := connection()
-	//if err != nil {
-	//	fmt.Errorf("Problem z polaczeniem %s", err.Error())
-	//	return
-	//}
-
-	//database.Where("id_studenta=?", id).Save(&user)
-	//c.Redirect(301, "/")
-
-}
-
-func studentAdd(c *gin.Context) {
-	var dodajSe = Students{}
-	err := c.ShouldBindJSON(&dodajSe)
-	if err != nil {
-		outs := Outs{
-			Status:     400,
-			Message:    "Podaj poprawny format danych",
-			UpdateRows: 0,
-			ErrorValue: err.Error(),
-		}
-		c.JSON(400, outs)
-		return
-	}
-	database, err := connection()
-	if err != nil {
-		fmt.Errorf("Problem z polaczeniem %s", err.Error())
-		return
-	}
-	result := database.Select("imie", "nazwisko", "data_urodzenia", "wydzial", "plec").Create(&dodajSe)
-	if result.Error != nil {
-		//sprawdz 400 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		c.JSON(400, Outs{
-			Status:     400,
-			Message:    "Problem z dodaniem użytkownika",
-			UpdateRows: result.RowsAffected,
-			ErrorValue: result.Error.Error(),
-		})
-	}
-	c.JSON(200, Outs{
-		Status:     200,
-		Message:    "Dodano użytkownika",
-		UpdateRows: result.RowsAffected,
-		ErrorValue: "",
-	})
-
-}
 
 func main() {
+	database,err:=connection()
+	if err!=nil{
+		log.Fatal("Problem z baza")
+		return
+	}
 	server := gin.Default()
-	server.GET("/", indexHandler)
-	server.DELETE("/", studentDelete)
-	server.PUT("/", studentChange)
-	server.POST("/", studentAdd)
+	server.Use(dbMiddleware(*database))
+	student:=server.Group("student")
+	{
+		student.GET("/", studenci.IndexHandler)
+		student.DELETE("/", studenci.StudentDelete)
+		student.PUT("/", studenci.StudentChange)
+		student.POST("/", studenci.StudentAdd)
+	}
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 	server.Run(":" + port)
+}
+
+func connection() (*gorm.DB,error) {
+	file := "uczelnia"
+	db,err:=gorm.Open(sqlite.Open(file), &gorm.Config{})
+	if err!=nil{
+		return nil,fmt.Errorf("Blad polaczenia z baza danych: %v",err.Error())
+	}
+	return db,nil
+}
+
+func dbMiddleware(db gorm.DB)gin.HandlerFunc{
+	return func(c *gin.Context){
+		c.Set("db",db)
+		c.Next()
+	}
 }
---
+++ studenci/students.go
@@ -0,0 +1,149 @@
+package studenci
+
+import(
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type Results struct {
+	Status       int
+	TotalResults int64
+	Student      []Students
+	ErrorCode	string
+}
+
+type Outs struct {
+	Status     int
+	Message    string
+	UpdateRows int64
+	ErrorCode string
+}
+
+type Students struct {
+	IDStudenta    int    `json:"idstudenta"`
+	Imie          string `json:"imiestudenta"`
+	Nazwisko      string `json:"nazwiskostudenta"`
+	DataUrodzenia string `json:"datastudenta"`
+	Wydzial       string `json:"wydzialstudenta"`
+	Plec          string `json:"plecstudenta"`
+}
+
+func outFunc(status int,mess string,rows int64,errc string,c *gin.Context){
+	outs := Outs{
+		Status:     status,
+		Message:    mess,
+		UpdateRows: rows,
+		ErrorCode: errc,
+	}
+	c.JSON(status, outs)
+}
+
+func (s *Students)compare(nowy *Students){
+	if nowy.Imie!="" && nowy.Imie!=s.Imie{
+		s.Imie=nowy.Imie
+	}
+	if nowy.Nazwisko!="" && nowy.Nazwisko!=s.Nazwisko{
+		s.Nazwisko=nowy.Nazwisko
+	}
+	if nowy.DataUrodzenia!="" && nowy.DataUrodzenia!=s.DataUrodzenia{
+		s.DataUrodzenia=nowy.DataUrodzenia
+	}
+	if nowy.Wydzial!="" && nowy.Wydzial!=s.Wydzial{
+		s.Wydzial=nowy.Wydzial
+	}
+	if nowy.Plec!="" && nowy.Plec!=s.Plec{
+		s.Plec=nowy.Plec
+	}
+
+}
+
+func IndexHandler(c *gin.Context) {
+	db,dbBool:=c.Get("db")
+	result:=Results{}
+	if dbBool != true{
+		result=Results{
+			Status: 500,
+			ErrorCode: "Database error",
+		}
+	}else
+	{
+		var studenci []Students
+		database:=db.(gorm.DB)
+		selectResult:=database.Find(&studenci)
+		result = Results{
+			Status:       200,
+			TotalResults: selectResult.RowsAffected,
+			Student:      studenci,
+		}
+	}
+	c.JSON(200, result)
+}
+
+func StudentDelete(c *gin.Context) {
+	var usunSe = Students{}
+	err := c.ShouldBindJSON(&usunSe)
+	if err != nil {
+		outFunc(400,"Podaj poprawny format danych",0,err.Error(),c)
+		return
+	}
+	db,dbBool:=c.Get("db")
+	if dbBool==false{
+		outFunc(500,"Nie znaleziono bazy danych",0,"Database error",c)
+		return
+	}
+	database:=db.(gorm.DB)
+	result := database.Delete(&Students{},usunSe.IDStudenta)
+	if result.Error != nil || result.RowsAffected==0 {
+		outFunc(400,"Problem z usunięciem studenta",result.RowsAffected,result.Error.Error(),c)
+		return
+	}
+	outFunc(200,"Usunięto studenta",result.RowsAffected,"",c)
+}
+
+func StudentChange(c *gin.Context) {
+	edytujSe := Students{}
+	err := c.ShouldBindJSON(&edytujSe)
+	if err!=nil{
+		outFunc(400,"Podaj poprawny format danych",0,err.Error(),c)
+		return
+	}
+	db,dbBool:=c.Get("db")
+	if dbBool==false{
+		outFunc(500,"Nie znaleziono bazy danych",0,"Database error",c)
+		return
+	}
+	database:=db.(gorm.DB)
+	student:=Students{}
+	if edytujSe.IDStudenta==0{
+		outFunc(400,"Niepoprawne idstudenta",0,"Invalid primarykey",c)
+		return
+	}
+	database.First(&student,edytujSe.IDStudenta)
+	student.compare(&edytujSe)
+
+	result:=database.Where("id_studenta=?", student.IDStudenta).Save(&student)
+	outFunc(200,"Zmieniono dane studenta",result.RowsAffected,"",c)
+
+}
+
+func StudentAdd(c *gin.Context) {
+	var dodajSe = Students{}
+	err := c.ShouldBindJSON(&dodajSe)
+	if err != nil {
+		outFunc(400,"Podaj poprawny format danych",0,err.Error(),c)
+		return
+	}
+	db,dbBool:=c.Get("db")
+	if dbBool==false{
+		outFunc(500,"Nie znaleziono bazy danych",0,"Database error",c)
+		return
+	}
+	database:=db.(gorm.DB)
+	result := database.Select("imie", "nazwisko", "data_urodzenia", "wydzial", "plec").Create(&dodajSe)
+	if result.Error != nil {
+		//sprawdz 400 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		outFunc(400,"Problem z dodaniem studenta",result.RowsAffected,result.Error.Error(),c)
+	}
+	outFunc(200,"Dodano studenta",result.RowsAffected,"",c)
+
+}
